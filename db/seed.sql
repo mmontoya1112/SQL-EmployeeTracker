96e4df641c2aa86829bfcad33e59d0785609554f
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-09-27 15:17:44 -0700

    added employees in seeds

--- db/seed.sql
+++ db/seed.sql
@@ -13,4 +13,26 @@ VALUES ('legal lead', 550000, 1),
     ('banker', 200000, 4),
     ('lead banker', 250000, 4);
 
-INSERT INTO 
+INSERT INTO employee (
+    firstName,
+    lastName,
+    titleID,
+    managerID,
+    managerConfirm
+)
+
+VALUES ('trixie', 'mattel', 1, null, true),
+    ('bob', 'the drag queen', 2, 2, false),
+    ('roxxxie', 'andrews', 2, 2, true),
+    ('gia', 'gunn', 3, 1, true),
+    ('yuhua', 'hamasaki', 3, null, false),
+    ('katya', 'zam', 4, 3, false),
+    ('tia', 'koffee', 4, null, true),
+    ('monet', 'exchange', 5, null, true),
+    ('plastique', 'tiara', 5, 4, false),
+    ('violet', 'chachki', 6, 4, false),
+    ('jinx', 'monsoon', 6, null, true);
+INSERT INTO manager (firstName, lastName)
+SELECT  firstName, lastName
+FROM employee
+WHERE managerConfirm = 1;
