e7631b0116b55db9d0a1c66495a0db987eef1ecd
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-09-27 15:05:47 -0700

    added titles to seeds

---
+++ db/seed.sql
@@ -0,0 +1,16 @@
+INSERT INTO department (name)
+VALUES ('engineering'),
+    ('finance'),
+    ('legal'),
+    ('sales')
+INSERT INTO titles (title, salary, departmentID)
+VALUES ('legal lead', 550000, 1),
+    ('lawyer', 200000, 1),
+    ('sales person', 100000, 2),
+    ('sales lead', 120000, 2),
+    ('engineer', 200000, 3),
+    ('lead engineer', 400000, 3),
+    ('banker', 200000, 4),
+    ('lead banker', 250000, 4);
+
+INSERT INTO 
