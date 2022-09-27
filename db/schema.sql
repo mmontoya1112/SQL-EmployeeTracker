e7420b6140c57f727fccbad4ed6bd48712f490bb
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-09-27 14:56:42 -0700

    added last table in schema

--- db/schema.sql
+++ db/schema.sql
@@ -20,13 +20,24 @@ CREATE TABLE department (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE roles (
+CREATE TABLE title (
     id  INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT,
+    departmentID INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (departmentID) REFERENCES department(id)
+);
 
+CREATE TABLE employee (
+    id  INT NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(30),
+    lastName VARCHAR(30),
+    titleID INT,
+    managerID INT,
+    managerConfirm BOOLEAN,
+    PRIMARY KEY (id),
+    FOREIGN KEY (titleID) REFERENCES titles(id),
+    FOREIGN KEY (managerID) REFERENCES titles(id) ON DELETE 
+    SET NULL
 )
-
