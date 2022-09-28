1dcb24ae4f1746e295ee0d2ca01396bd96af5a98
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-09-27 18:05:34 -0700

    added ability to see employees by department

--- library/employee.js
+++ library/employee.js
@@ -30,3 +30,47 @@ const viewEmployees = () => {
         }
     );
 };
+
+const employeesByDep = () => {
+    connection.query(
+        `SELECT * FROM department`,
+        function (err, results, fields) {
+            if (err) {
+                console.log(err.message);
+                return;
+                depArray = [];
+                results.forEach(item => {
+                    depArray.push(item.name)
+                });
+                inquirer
+                .prompt({
+                    type: 'list',
+                    name: 'filterByDep',
+                    message: 'filter by department',
+                    choices: depArray
+                })
+                .then((data) => {
+                    connection.query(
+                        `SELECT employee.id, employee.firsName, employee.lastName, department.name AS department
+                        FROM employee
+                        LEFT JOIN roles
+                        ON employee.title_id = titles.id
+                        LEFT JOIN department
+                        ON titles.department_id = department.id
+                        WHERE department.name = ?`,
+                        [data['filterByDep']],
+                        function (err, results, fields) {
+                            if (err) {
+                                console.log(err.message);
+                                return;
+                            }
+                            console.table(results);
+                            promptUser();
+                        }
+
+                    )
+                });
+            }
+        }
+    );
+};
